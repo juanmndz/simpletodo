2de2df535b2405589dc5cd88d1add1c24846e5a7
Juan Mendez <juanmndz19@gmail.com>
2017-05-19 11:33:26 -0700

    Initial Commit

---
+++ src/store.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import { createLogger } from 'redux-logger';
+import rootReducer from './reducers';
+
+const middlewares = [
+  createLogger()
+]
+
+export default createStore(
+  rootReducer,
+  undefined,
+  compose(
+    applyMiddleware(...middlewares),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+)
