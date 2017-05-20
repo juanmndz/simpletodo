12d9362dc3c7e99620741414ced6862742770197
Juan Mendez <juanmndz19@gmail.com>
2017-05-19 18:35:06 -0700

    Add json api server

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { createLogger } from 'redux-logger';
 import rootReducer from './reducers';
+import thunk from 'redux-thunk';
 
 const middlewares = [
   createLogger()
